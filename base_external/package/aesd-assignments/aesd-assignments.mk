a6fb5848622902846a664326f8bb3fb2fd9bdeb9
cyboflash <2373916+cyboflash@users.noreply.github.com>
2026-04-03 00:26:37 -0700

    completed assignment 4

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 1493dcd8625968a3670005927c99fdeb26fd165d
+AESD_ASSIGNMENTS_VERSION = 28f9f6e64250f9a212f7e8c7b842ed21b671f77f
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
